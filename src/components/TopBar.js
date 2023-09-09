53c02c2ba9e63f5626479b91810fc544efff5b46
Walter <walter19@connect.hku.hk>
2023-09-09 18:04:08 +0800

    fix routing

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -3,14 +3,14 @@ import { NavLink } from 'react-router-dom'
 function TopBar() {
   return (
     <div className="TopBar">
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/study_plan">Course Planner</NavLink>
-      <NavLink to="/view_program">View Major Minor</NavLink>
-      <NavLink to="/add_course">Add Course</NavLink>
-      <NavLink to="/add_degree">Add Degree</NavLink>
-      <NavLink to="/add_major_minor">Add Major Minor</NavLink>
-      <NavLink to="/upload_export_file">Upload Export File</NavLink>
-      <NavLink to="/time_table">Time Table</NavLink>
+      <NavLink to="/Course-Planner-Host-Fork/">Home</NavLink>
+      <NavLink to="/Course-Planner-Host-Fork/study_plan">Course Planner</NavLink>
+      <NavLink to="/Course-Planner-Host-Fork/view_program">View Major Minor</NavLink>
+      <NavLink to="/Course-Planner-Host-Fork/add_course">Add Course</NavLink>
+      <NavLink to="/Course-Planner-Host-Fork/add_degree">Add Degree</NavLink>
+      <NavLink to="/Course-Planner-Host-Fork/add_major_minor">Add Major Minor</NavLink>
+      <NavLink to="/Course-Planner-Host-Fork/upload_export_file">Upload Export File</NavLink>
+      <NavLink to="/Course-Planner-Host-Fork/time_table">Time Table</NavLink>
     </div>
   )
 }
