957e4df44814d2c3b87b007da7e2fbcc0426841e
Walter <walter19@connect.hku.hk>
2024-01-04 20:28:24 +0800

    update home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,15 +6,16 @@ function HomePage() {
         different course.
       </div>
       <div>
-        To use please download the config from "link" (not offical and inputed
-        by manpower, please refer to the offical syllabuses for your final study
-        plan)
+        There is a config default in the website, but you may refer to https://github.com/Walter-Tong/Course_Planner_Config for a maybe more update config. (not offical and inputed by manpower, please refer to the offical syllabuses for your final study plan)
       </div>
       <div>
-        Also please notice the app can not display all requirement of courses
+        Also please notice the app can not display all requirement of courses (mainly the course planner part)
       </div>
       <div>
         The idea of the Time Table is inspired by hovergecko
+      </div>
+      <div>
+        Our github repo: https://github.com/ivan8882004/HKUCoursePlanner-Reviewer
       </div>
     </div>
   )
