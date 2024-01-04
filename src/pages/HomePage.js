83694dba0df6983ac16ec5d8f376d5e1b4624744
Walter <walter19@connect.hku.hk>
2024-01-04 20:40:53 +0800

    fix hyperlink

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,17 +5,21 @@ function HomePage() {
         This is a web app for handle the complex prereg and exclusive for
         different course.
       </div>
+      <br />
       <div>
-        There is a config default in the website, but you may refer to <a ref={"https://github.com/Walter-Tong/Course_Planner_Config"}>link</a> for a maybe more update config. (not offical and inputed by manpower, please refer to the offical syllabuses for your final study plan)
+        There is a config default in the website, but you may refer to <a href={"https://github.com/Walter-Tong/Course_Planner_Config"} target="_blank" rel="noreferrer">link</a> for a maybe more update config. <br />(not offical and inputed by manpower, please refer to the offical syllabuses for your final study plan)
       </div>
+      <br />
       <div>
         Also please notice the app can not display all requirement of courses (mainly the course planner part)
       </div>
+      <br />
       <div>
         The idea of the Time Table is inspired by hovergecko
       </div>
+      <br />
       <div>
-        <a ref={"https://github.com/ivan8882004/HKUCoursePlanner-Reviewer"}>Our github repo</a>
+        <a href={"https://github.com/ivan8882004/HKUCoursePlanner-Reviewer"} target="_blank" rel="noreferrer">Our github repo</a>
       </div>
     </div>
   )
