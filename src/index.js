761b69f0f7937d038f8a87fd3d2071c20203278f
Kenneth Kwan <123370597+kennethkn@users.noreply.github.com>
2024-01-31 23:21:59 +0800

    Add GA4 & remove junks

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import { Provider } from 'react-redux'
 import { store } from './store'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
+import ReactGA from 'react-ga4'
+
+ReactGA.initialize('G-1Q2G26WWE7')
 
 const el = document.getElementById('root')
 
